4ece3c8fc098271e368e5f9f8adea1cc973b0a6b
Sam Behrens <sbehrens@smith.edu>
2015-11-28 20:30:13 -0500

    adding wikipedia history

---
+++ USScraper.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+import re 
+
+r  = requests.get("https://en.wikipedia.org/wiki/Timeline_of_United_States_history#20th_century")
+data = r.text
+soup = BeautifulSoup(data)
+
+
+# invalid_tags = ['b', 'i', 'u']
+
+
+# for x in soup.find_all():
+# 	if len(x.text) == 0:
+# 		x.extract()
+
+
+# for tag in invalid_tags: 
+# 	for match in soup.findAll(tag):
+# 		match.replaceWithChildren()
+
