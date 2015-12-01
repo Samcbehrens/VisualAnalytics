cc59fc8b189dd72f868eead5a44ab6404113a104
Sam Behrens <sbehrens@smith.edu>
2015-11-30 20:47:21 -0500

    trying to format in the correct fashion to show up on timeline

--- dataProcessing/timeline3.py
+++ dataProcessing/timeline3.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import re 
+import json
+import random
 
 r  = requests.get("http://herb.ashp.cuny.edu/items/show/1285")
 data = r.text
@@ -18,4 +20,30 @@ soup = str(soup)
 soup = soup.replace('<strong>', "-")
 soup = soup.replace('</strong>', "-")
 finalOutput = soup.split('-')
+
+for n in range(0,4):
+	finalOutput[n]=""
+
 print finalOutput
+print
+print 
+
+colors = ["red","orange", "yellow", "green", "blue"]
+timeline = {"label": "timeline3", "times": []}
+addEvent={"color":"blue", "label":"description", "start_time": 1, "end_time": 2}
+
+for n in finalOutput:
+	if n.find("\n\n\n") >=0:
+		n.replace("\n\n\n","")
+		addEvent["label"] = n
+	else:
+		addEvent["start_time"] = n
+
+	if addEvent["label"]!="description" && addEvent["start_time"] != 1:
+		randomNum = random.randint(0,4)
+		addEvent["color"]=colors[randomNum]
+		timeline["times"].append(addEvent)
+		addEvent={"color":"blue", "label":"description", "start_time": 1, "end_time": 2}
+
+
+
