e8a250388e60fcb547a1919d427603ec6f0c3212
Sam Behrens <sbehrens@smith.edu>
2015-12-07 22:12:38 -0500

    fucked

--- dataProcessing/USTimeline.py
+++ dataProcessing/USTimeline.py
@@ -6,4 +6,26 @@ with open('usTimeline.csv', 'rb') as csvfile:
 	for row in reader:
 		allInformation.append(row)
 
-print allInformation
+
+newFormation =[]
+
+for i in range(0, len(allInformation)):
+	if i+1 < len(allInformation)-2:
+		if len(allInformation[i+1])<4:
+			allInformation[i].append(allInformation[i+1])
+			newFormation.append(allInformation[i])
+
+	thisPotYear = allInformation[i][0]
+	print(thisPotYear.isdigit())
+	print thisPotYear
+	print type(thisPotYear)
+	if thisPotYear.isdigit():
+		newFormation.append(allInformation[i])
+	if allInformation[i][0]=='':
+		year = newFormation[len(newFormation)-1][0]
+		allInformation[i][0] = year
+		newFormation.append(allInformation[i])
+
+for n in newFormation:	
+	print n
+
