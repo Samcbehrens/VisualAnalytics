1ef01668e97f7e4f6962ec63151ed426df9945eb
Sam Behrens <sbehrens@smith.edu>
2015-12-07 23:01:26 -0500

    the reader works now just need to reformat into json

--- dataProcessing/USTimeline.py
+++ dataProcessing/USTimeline.py
@@ -1,33 +1,42 @@
 import csv 
-allInformation = []
 
- with open('usTimeline.csv', 'rb') as csvfile:
-	dialect = csv.Sniffer().sniff(csvfile.read(), delimiters=',')
-	csvfile.seek(0)
-	reader=csv.reader(csvfile,dialect)
-	for line in reader:
-		allInformation.append(line)
+def readCsv():
 
-newFormation =[]
+	allInformation = []
 
-for i in range(0, len(allInformation)):
-	print allInformation[i]
-# 	if i+1 < len(allInformation)-2:
-# 		if len(allInformation[i+1])<4:
-# 			allInformation[i].append(allInformation[i+1])
-# 			newFormation.append(allInformation[i])
+	with open('usTimeline.csv', 'rb') as csvfile:
+		dialect = csv.Sniffer().sniff(csvfile.read(), delimiters=',')
+		csvfile.seek(0)
+		reader=csv.reader(csvfile,dialect)
+		for line in reader:
+			allInformation.append(line)
+	return allInformation
 
-# 	thisPotYear = allInformation[i][0]
-# 	print(thisPotYear.isdigit())
-# 	print thisPotYear
-# 	print type(thisPotYear)
-# 	if thisPotYear.isdigit():
-# 		newFormation.append(allInformation[i])
-# 	if allInformation[i][0]=='':
-# 		year = newFormation[len(newFormation)-1][0]
-# 		allInformation[i][0] = year
-# 		newFormation.append(allInformation[i])
+def reformat(allInformation):
 
-# for n in newFormation:	
-# 	print n
+	newFormation =[]
+
+	for i in range(0, len(allInformation)):
+
+		
+		## get index out of range if you dont check this first
+		if i+1 < len(allInformation)-1:
+			##look ahead to see if the next one doesnt have a date
+			if allInformation[i+1][0]=='':
+				allInformation[i+1][0]=allInformation[i][0]
+
+		#add if it has the correct date
+		thisPotYear = allInformation[i][0]
+		if thisPotYear.isdigit():
+			newFormation.append(allInformation[i])
+
+	return newFormation
+
+
+if __name__ == '__main__':
+	allInformation = readCsv()
+	newFormation = reformat(allInformation)
+
+	for n in newFormation:	
+		print n
 
