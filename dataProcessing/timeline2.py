b85fcc2469ae587cde583da43b869cbad7e3c0b9
Sam Behrens <sbehrens@smith.edu>
2015-12-10 21:49:43 -0500

    added svg to the timeline

--- dataProcessing/timeline2.py
+++ dataProcessing/timeline2.py
@@ -34,7 +34,7 @@ def webToJson(soup):
 	## formatting to turn into correct json 
 
 	colors = ["red","orange", "yellow", "green", "blue"]
-	timeline = {"label": "timeline3", "times": []}
+	timeline = {"label": "timeline2", "times": []}
 	addEvent={"color":"blue", "description":"description", "starting_time": 1}
 
 	## Must be in a certain format have to put in a array and then a set...crying 
